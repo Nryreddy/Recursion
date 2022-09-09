0dbc9055ca489acb084ea445c4ffff2580df80f7
Nry <nitheshm.19.becs@acharya.ac.in>
2022-09-09 23:35:45 +0530

    updated decitobin

--- deci_to_bin.py
+++ deci_to_bin.py
@@ -1,19 +1,17 @@
-def deciToBin(n):
-
-    if n>=1:
-        deciToBin(n//2)
-    print(n%2,end='')
-
 def deciToBin1(n):
-
+    assert int(n) == n,'the parameter must be an integer only'
     if n==0:
         return 0
-    return n%2 + 10 * deciToBin1(n//2)
+    return n%2 + 10 * deciToBin1(int(n//2))
 
+#def deciToBin(n):
+#
+#     if n>=1:
+#         deciToBin(n//2)
+#     print(n%2,end='')
+# print(deciToBin(10))
 
-print(deciToBin(10))
-
-print(deciToBin1(10))
+print(deciToBin1(100))
 
 
 
